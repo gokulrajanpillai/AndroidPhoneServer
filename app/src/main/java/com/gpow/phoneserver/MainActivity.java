384ab3c0f35c8f9f94c6b4591973248b99577f0b
gokulrajanpillai <gokulrajanpillai@gmail.com>
2022-05-22 23:22:34 +0200

    Implemented button flip with imagebutton

--- app/src/main/java/com/gpow/phoneserver/MainActivity.java
+++ app/src/main/java/com/gpow/phoneserver/MainActivity.java
@@ -2,26 +2,39 @@ package com.gpow.phoneserver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton pwrBtn;
+    private LinearLayout pwrBtnLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pwrBtn.findViewById(R.id.pwr_btn);
-        pwrBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                pwrBtn.setBackgroundColor(R.color.black);
-                new Toast()
+        pwrBtn = findViewById(R.id.pwr_btn);
+//        pwrBtnLayout = findViewById(R.id.pwr_btn_layout);
+        pwrBtn.setOnClickListener(view -> {
+            ColorDrawable colorDrawable = (ColorDrawable) pwrBtn.getBackground();
+            if (colorDrawable instanceof ColorDrawable) {
+                int color = ((ColorDrawable) colorDrawable).getColor();
+                if (color == getResources().getColor(R.color.green)) {
+                    pwrBtn.setBackgroundColor(getResources().getColor(R.color.red));
+                    Toast.makeText(MainActivity.this, "Stopped Server!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    pwrBtn.setBackgroundColor(getResources().getColor(R.color.green));
+                    Toast.makeText(MainActivity.this, "Started Server!",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
