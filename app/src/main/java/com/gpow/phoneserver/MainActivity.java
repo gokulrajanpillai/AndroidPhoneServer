d0eabac227ef34ef8b2eaf62bbf30ae5313e0e91
gokulrajanpillai <gokulrajanpillai@gmail.com>
2022-06-28 04:40:00 +0530

    Added new power button icon, Added bottom bar height to dimension, Displaying toasts without delay

--- app/src/main/java/com/gpow/phoneserver/MainActivity.java
+++ app/src/main/java/com/gpow/phoneserver/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -16,41 +17,92 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton pwrBtn;
     private TextView htmlCodeTv;
 
+    private int PORT_NUMBER = 8080;
     private PhoneServer server = null;
+
+    private Toast toast = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureHtmlTv();
         configureServerBtn();
     }
 
+
+    private void configureHtmlTv() {
+        htmlCodeTv = findViewById(R.id.htmlcode_tv);
+        String htmlcode = "<html>\n"
+                + "<body>\n"
+                + "<p>Hello World!</p>\n"
+                + "</body>\n"
+                + "</html>";
+        htmlCodeTv.setText(htmlcode);
+        htmlCodeTv.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    restartServer();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
     private void configureServerBtn() {
         pwrBtn = findViewById(R.id.pwr_btn);
-//        pwrBtnLayout = findViewById(R.id.pwr_btn_layout);
         pwrBtn.setOnClickListener(view -> {
             ColorDrawable colorDrawable = (ColorDrawable) pwrBtn.getBackground();
             if (colorDrawable instanceof ColorDrawable) {
                 int color = ((ColorDrawable) colorDrawable).getColor();
                 if (color == getResources().getColor(R.color.green)) {
                     pwrBtn.setBackgroundColor(getResources().getColor(R.color.red));
-                    Toast.makeText(MainActivity.this, "Stopped Server!",
-                            Toast.LENGTH_SHORT).show();
-                    if (server != null) {
-                        server.stopServer();
-                    }
+                    showToast("Stopped Server!");
+                    stopServer();
                 } else {
                     pwrBtn.setBackgroundColor(getResources().getColor(R.color.green));
-                    Toast.makeText(MainActivity.this, "Started Server!",
-                            Toast.LENGTH_SHORT).show();
-                    // Starting server
-                    if (server == null) {
-                        server = new PhoneServer(8080);
-                    }
-                    server.setWebsiteHTML(htmlCodeTv.getText().toString());
-                    server.startServer();
+                    showToast("Started Server!");
+                    startServer();
                 }
             }
         });
+    }
+
+
+    private void startServer() {
+        // Starting server
+        if (server == null) {
+            server = new PhoneServer(PORT_NUMBER);
+        }
+        server.setWebsiteHTML(htmlCodeTv.getText().toString());
+        server.startServer();
+    }
+
+
+    private void stopServer() {
+        if (server != null) {
+            server.stopServer();
+        }
+    }
+
+
+    private void restartServer() {
+        stopServer();
+        startServer();
+        showToast("Restarting server");
+    }
+
+
+    private void showToast(String msg) {
+        if (toast != null) {
+            toast.cancel();
+        }
+        toast = Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
